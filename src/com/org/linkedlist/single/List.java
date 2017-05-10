b60f269474cc60ac58eb935d211ad5583639fbe7
iamsubho76 <iamsubho76@gmail.com>
2017-05-10 10:29:20 +0530

    Added the classes of Single and Doyble Linked List

---
+++ src/com/org/linkedlist/single/List.java
@@ -0,0 +1,8 @@
+package com.org.linkedlist.single;
+
+public interface List<T> {
+    public void insert(T data);
+    public void remove(T data);
+    public void traverseList();
+    public int size();
+}
---
+++ src/com/org/linkedlist/single/TestSingleLinkedList.java
@@ -0,0 +1,20 @@
+package com.org.linkedlist.single;
+
+public class TestSingleLinkedList {
+
+    public static void main(String[] args) {
+
+        List<Integer> myLinkedList = new LinkedList<>();
+        
+        myLinkedList.insert(1);
+        myLinkedList.insert(2);
+        myLinkedList.insert(3);
+        myLinkedList.insert(4);
+        
+        myLinkedList.remove(1);
+        
+        
+        myLinkedList.traverseList();
+
+    }
+}
